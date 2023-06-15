c7f83a9cd11b8bbe717c764508a7780703488ff9
DariaTiessova <bermuds@inbox.ru>
2023-06-15 22:01:12 +0600

    AppsFlyer

---
+++ Assets/Scripts/StartGame.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+using UnityEngine.Serialization;
+using AppsFlyerSDK;
+using Infrastructure.Level;
+using UnityEngine.UI;
+
+public class StartGame : MonoBehaviour
+{
+    [FormerlySerializedAs("LevelLoader")]
+    public SceneLoader _sceneLoader;
+    [SerializeField]
+    private Text TextData;
+    [SerializeField] private Button continueButton;
+  
+    private void Start()
+    {
+        AppsFlyer.UnityCallBack += OnConversionDataReceived;
+        continueButton.onClick.AddListener(NextScene);
+    }
+
+    private void NextScene()
+    {
+        _sceneLoader.StartGame();
+    }
+
+    private void OnConversionDataReceived(string conversionData)
+    {
+        Debug.Log("ConversionData: " + conversionData);
+        TextData.text = conversionData;
+    }
+}
+
